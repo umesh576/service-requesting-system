5bb607a1615dd032722f253d5c9a1be5e1577cd8
Umesh Joshi <uj19343@gmail.com>
2025-12-08 22:03:00 +0545

    new project

--- src/main/java/com/example/serviceFinal/controller/UserController.java
+++ src/main/java/com/example/serviceFinal/controller/UserController.java
@@ -1,3 +1,25 @@
 package com.example.serviceFinal.controller;
 
-public class UserController {}
+import com.example.serviceFinal.entity.User;
+import java.util.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+  @PostMapping("/add")
+  public String postMethodName(@RequestBody String entity) {
+    //TODO: process POST request
+
+    return entity;
+  }
+  // @GetMapping("/")
+  // public List<User> getUser() {
+  //   return "umesh joshi";
+  // }
+}
